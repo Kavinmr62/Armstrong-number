02600bb8f22aa19a013bcc028e39f9c6d6d3344d
Kavin M R <154052561+Kavinmr62@users.noreply.github.com>
2025-05-28 07:38:42 +0530

    Create armstrong.java

---
+++ armstrong.java
@@ -0,0 +1,26 @@
+import java.util.*;
+public class Main{
+    public static void main(String[] args){
+        Scanner kk=new Scanner(System.in);
+        int n=kk.nextInt();
+        int c=0,temp=n,t=n,sum=0;
+        while(n>0)
+        {
+            c++;
+            n/=10;
+        }
+        while(temp>0)
+        {
+            int rem=temp%10;
+            int l=c,p=1;
+            while(l>0)
+            {
+                p=p*rem;
+                l--;
+            }
+            sum=sum+p;
+            temp=temp/10;
+        }
+        System.out.println((sum==t)?"armstrong":"not armstrong");
+    }
+}
